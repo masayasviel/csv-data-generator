2ef54ae44980d4a12aaa585b543cb7da6ae8c508
masayasviel <33654883+masayasviel@users.noreply.github.com>
2023-06-24 15:31:55 +0900

    :test_tube: prettierテスト

---
+++ src/organisms/choice-random.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { InputTemplate } from '../templates/input-temp';
+
+export const ChoiceRandom: React.FC = () => {
+  const TITLE = '選択肢';
+  return (
+    <InputTemplate title={TITLE}>
+      <div>aaaa</div>
+    </InputTemplate>
+  );
+};
